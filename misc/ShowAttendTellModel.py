3187bb9cb7baf08271c2681981c860f4ed908065
Maha <maha.elbayad@gmail.com>
2017-12-12 15:56:54 +0100

    auto up

--- misc/ShowAttendTellModel.py
+++ misc/ShowAttendTellModel.py
@@ -9,6 +9,167 @@ from torch.autograd import *
 from misc.Decoder import DecoderModel
 
 
+class AdaAtt_lstm(nn.Module):
+    def __init__(self, opt, use_maxout=True):
+        super(AdaAtt_lstm, self).__init__()
+        self.input_encoding_size = opt.input_encoding_size
+        #self.rnn_type = opt.rnn_type
+        self.rnn_size = opt.rnn_size
+        self.num_layers = opt.num_layers
+        self.drop_prob_lm = opt.drop_prob_lm
+        self.fc_feat_size = opt.fc_feat_size
+        self.att_feat_size = opt.att_feat_size
+        self.att_hid_size = opt.att_hid_size
+
+        self.use_maxout = use_maxout
+
+        # Build a LSTM
+        self.w2h = nn.Linear(self.input_encoding_size, (4+(use_maxout==True)) * self.rnn_size)
+        self.v2h = nn.Linear(self.rnn_size, (4+(use_maxout==True)) * self.rnn_size)
+
+        self.i2h = nn.ModuleList([nn.Linear(self.rnn_size, (4+(use_maxout==True)) * self.rnn_size) for _ in range(self.num_layers - 1)])
+        self.h2h = nn.ModuleList([nn.Linear(self.rnn_size, (4+(use_maxout==True)) * self.rnn_size) for _ in range(self.num_layers)])
+
+        # Layers for getting the fake region
+        if self.num_layers == 1:
+            self.r_w2h = nn.Linear(self.input_encoding_size, self.rnn_size)
+            self.r_v2h = nn.Linear(self.rnn_size, self.rnn_size)
+        else:
+            self.r_i2h = nn.Linear(self.rnn_size, self.rnn_size)
+        self.r_h2h = nn.Linear(self.rnn_size, self.rnn_size)
+
+
+    def forward(self, xt, img_fc, state):
+
+        hs = []
+        cs = []
+        for L in range(self.num_layers):
+            # c,h from previous timesteps
+            prev_h = state[0][L]
+            prev_c = state[1][L]
+            # the input to this layer
+            if L == 0:
+                x = xt
+                i2h = self.w2h(x) + self.v2h(img_fc)
+            else:
+                x = hs[-1]
+                x = F.dropout(x, self.drop_prob_lm, self.training)
+                i2h = self.i2h[L-1](x)
+
+            all_input_sums = i2h+self.h2h[L](prev_h)
+
+            sigmoid_chunk = all_input_sums.narrow(1, 0, 3 * self.rnn_size)
+            sigmoid_chunk = F.sigmoid(sigmoid_chunk)
+            # decode the gates
+            in_gate = sigmoid_chunk.narrow(1, 0, self.rnn_size)
+            forget_gate = sigmoid_chunk.narrow(1, self.rnn_size, self.rnn_size)
+            out_gate = sigmoid_chunk.narrow(1, self.rnn_size * 2, self.rnn_size)
+            # decode the write inputs
+            if not self.use_maxout:
+                in_transform = F.tanh(all_input_sums.narrow(1, 3 * self.rnn_size, self.rnn_size))
+            else:
+                in_transform = all_input_sums.narrow(1, 3 * self.rnn_size, 2 * self.rnn_size)
+                in_transform = torch.max(\
+                    in_transform.narrow(1, 0, self.rnn_size),
+                    in_transform.narrow(1, self.rnn_size, self.rnn_size))
+            # perform the LSTM update
+            next_c = forget_gate * prev_c + in_gate * in_transform
+            # gated cells form the output
+            tanh_nex_c = F.tanh(next_c)
+            next_h = out_gate * tanh_nex_c
+            if L == self.num_layers-1:
+                if L == 0:
+                    i2h = self.r_w2h(x) + self.r_v2h(img_fc)
+                else:
+                    i2h = self.r_i2h(x)
+                n5 = i2h+self.r_h2h(prev_h)
+                fake_region = F.sigmoid(n5) * tanh_nex_c
+
+            cs.append(next_c)
+            hs.append(next_h)
+
+        # set up the decoder
+        top_h = hs[-1]
+        top_h = F.dropout(top_h, self.drop_prob_lm, self.training)
+        fake_region = F.dropout(fake_region, self.drop_prob_lm, self.training)
+
+        state = (torch.cat([_.unsqueeze(0) for _ in hs], 0),
+                torch.cat([_.unsqueeze(0) for _ in cs], 0))
+        return top_h, fake_region, state
+
+class AdaAtt_attention(nn.Module):
+    def __init__(self, opt):
+        super(AdaAtt_attention, self).__init__()
+        self.input_encoding_size = opt.input_encoding_size
+        #self.rnn_type = opt.rnn_type
+        self.rnn_size = opt.rnn_size
+        self.drop_prob_lm = opt.drop_prob_lm
+        self.att_hid_size = opt.att_hid_size
+
+        # fake region embed
+        self.fr_linear = nn.Sequential(
+            nn.Linear(self.rnn_size, self.input_encoding_size),
+            nn.ReLU(),
+            nn.Dropout(self.drop_prob_lm))
+        self.fr_embed = nn.Linear(self.input_encoding_size, self.att_hid_size)
+
+        # h out embed
+        self.ho_linear = nn.Sequential(
+            nn.Linear(self.rnn_size, self.input_encoding_size),
+            nn.Tanh(),
+            nn.Dropout(self.drop_prob_lm))
+        self.ho_embed = nn.Linear(self.input_encoding_size, self.att_hid_size)
+
+        self.alpha_net = nn.Linear(self.att_hid_size, 1)
+        self.att2h = nn.Linear(self.rnn_size, self.rnn_size)
+
+    def forward(self, h_out, fake_region, conv_feat, conv_feat_embed):
+
+        # View into three dimensions
+        att_size = conv_feat.numel() // conv_feat.size(0) // self.rnn_size
+        conv_feat = conv_feat.view(-1, att_size, self.rnn_size)
+        conv_feat_embed = conv_feat_embed.view(-1, att_size, self.att_hid_size)
+
+        # view neighbor from bach_size * neighbor_num x rnn_size to bach_size x rnn_size * neighbor_num
+        fake_region = self.fr_linear(fake_region)
+        fake_region_embed = self.fr_embed(fake_region)
+
+        h_out_linear = self.ho_linear(h_out)
+        h_out_embed = self.ho_embed(h_out_linear)
+
+        txt_replicate = h_out_embed.unsqueeze(1).expand(h_out_embed.size(0), att_size + 1, h_out_embed.size(1))
+
+        img_all = torch.cat([fake_region.view(-1,1,self.input_encoding_size), conv_feat], 1)
+        img_all_embed = torch.cat([fake_region_embed.view(-1,1,self.input_encoding_size), conv_feat_embed], 1)
+
+        hA = F.tanh(img_all_embed + txt_replicate)
+        hA = F.dropout(hA,self.drop_prob_lm, self.training)
+
+        hAflat = self.alpha_net(hA.view(-1, self.att_hid_size))
+        PI = F.softmax(hAflat.view(-1, att_size + 1))
+
+        visAtt = torch.bmm(PI.unsqueeze(1), img_all)
+        visAttdim = visAtt.squeeze(1)
+
+        atten_out = visAttdim + h_out_linear
+
+        h = F.tanh(self.att2h(atten_out))
+        h = F.dropout(h, self.drop_prob_lm, self.training)
+        return h
+
+
+class AdaAttCore(nn.Module):
+    def __init__(self, opt, use_maxout=False):
+        super(AdaAttCore, self).__init__()
+        self.lstm = AdaAtt_lstm(opt, use_maxout)
+        self.attention = AdaAtt_attention(opt)
+
+    def forward(self, xt, fc_feats, att_feats, p_att_feats, state):
+        h_out, p_out, state = self.lstm(xt, fc_feats, state)
+        atten_out = self.attention(h_out, p_out, att_feats, p_att_feats)
+        return atten_out, state
+
+
 class ShowAttendTellModel(DecoderModel):
 
     def __init__(self, opt):
@@ -26,6 +187,9 @@ class ShowAttendTellModel(DecoderModel):
         self.att_embed = nn.Sequential(nn.Linear(self.att_feat_size, self.rnn_size),
                                        nn.ReLU(),
                                        nn.Dropout(self.drop_prob_lm))
+        # The core RNN
+        self.core = AdaAttCore(opt)
+
         self.logit = nn.Linear(self.rnn_size, self.vocab_size)
         self.ctx2att = nn.Linear(self.rnn_size, self.att_hid_size)
         self.init_weights()
