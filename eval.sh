39ac8575e60229541a87cb7d35a15f81e66f2ae8
redgns <maha.elbayad@gmail.com>
2018-01-23 19:03:18 +0100

    auto up

--- eval.sh
+++ eval.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 model1=$1
 
-oarsub -l "walltime=24:0:0" -n eval_$1 \
+oarsub -l "walltime=1:0:0" -n eval_$1 \
        -t besteffort -t idempotent \
        -p "not gpumodel='k40m' and gpumem>6000" \
        -O  eval_stdout -E eval_stderr \
