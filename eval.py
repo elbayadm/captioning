fb19a7f35de196378d70136abca9178ef7fd6c90
Maha ELBAYAD <maha.elbayad@gmail.com>
2018-01-31 09:14:49 +0100

    auto up

--- eval.py
+++ eval.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
                   "start_from_best", "language_eval", "logger",
                   "val_images_use", 'input_data', "loss_version", "region_size",
                   "use_adaptive_pooling", "clip_reward",
-                  "gpu_id", "max_epochs"]
+                  "gpu_id", "max_epochs", "modelname"]
         for k in list(vars(infos['opt']).keys()):
             if k not in ignore and "learning" not in k:
                 if k in vars(opt):
